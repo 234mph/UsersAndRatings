6feea0addc7f4f2c72268a01bce058d72a5aa3d0
daniil <daniilkienko@mail.ru>
2024-10-01 17:13:09 +0500

    initial commit

---
+++ src/main/java/com/example/UsersAndRatingsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAndRatingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersAndRatingsApplication.class, args);
+	}
+
+}
